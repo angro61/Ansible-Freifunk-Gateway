669b63314e19c967778dea5c55029698e9c1f748
Claus Marxmeier <claus@marxmeier.de>
2018-12-03 16:47:38 +0100

    Update yanic_config_validator.sh

--- services_yanic/files/yanic_config_validator.sh
+++ services_yanic/files/yanic_config_validator.sh
@@ -2,5 +2,6 @@
 for bat in `systemctl | grep yanic | grep bat | grep active | awk  '{print $5}' | cut -d '_' -f 2`; do
     echo "config file for $bat is: "
     ls /etc/yanic/yanic_$bat.conf
-    echo "fe80 address is: "`ifconfig $bat | grep fe80 | awk  '{print $2}' | cut -d '/' -f 1`
+    echo "configured ip_adress is: "`cat /etc/yanic/yanic_$bat.conf | grep ip_address | cut -d '=' -f 2`
+    echo "fe80 current address is: "`ifconfig $bat | grep fe80 | awk  '{print $2}' | cut -d '/' -f 1`
 done
