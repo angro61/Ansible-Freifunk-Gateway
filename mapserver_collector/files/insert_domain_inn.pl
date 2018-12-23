562f36b2228341b0a4082a72669863d301f4f913
Claus Marxmeier <claus@marxmeier.de>
2018-12-23 17:23:12 +0100

    Update insert_domain_inn.pl

--- mapserver_collector/files/insert_domain_inn.pl
+++ mapserver_collector/files/insert_domain_inn.pl
@@ -6,6 +6,6 @@ use warnings;
 $^I = '.bak'; # create a backup copy
 
 while (<>) {
-   s/\"site_code\"\:\"inn\"/\"domain_code\"\:\"tdf-inn\",\"site_code\"\:\"tdf-inn\"/g; # do the replacement
+   s/\"site_code\"\:\"inn\"/\"domain_code\"\:\"inn\",\"site_code\"\:\"inn\"/g; # do the replacement
    print; # print to the modified file
 }
