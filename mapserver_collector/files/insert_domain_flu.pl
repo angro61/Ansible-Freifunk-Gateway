e241528d0d5dc783ad75e06903bfa97bcfea4221
Claus Marxmeier <claus@marxmeier.de>
2018-12-23 18:09:43 +0100

    Update insert_domain_flu.pl

--- mapserver_collector/files/insert_domain_flu.pl
+++ mapserver_collector/files/insert_domain_flu.pl
@@ -6,6 +6,7 @@ use warnings;
 $^I = '.bak'; # create a backup copy
 
 while (<>) {
+   s/\"system\"\:\{\}/\"system\"\:\{\"site_code\"\:\"flu\"\}/g; # add site_code to empty records
    s/\"site_code\"\:\"flu\"/\"domain_code\"\:\"flu\",\"site_code\"\:\"flu\"/g; # do the replacement
    print; # print to the modified file
 }
