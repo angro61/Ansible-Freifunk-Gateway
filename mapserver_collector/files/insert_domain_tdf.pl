d5d0708a792b40fb2b110035831117459f77dd73
Claus Marxmeier <claus@marxmeier.de>
2018-12-23 17:54:22 +0100

    Update insert_domain_tdf.pl

--- mapserver_collector/files/insert_domain_tdf.pl
+++ mapserver_collector/files/insert_domain_tdf.pl
@@ -6,7 +6,7 @@ use warnings;
 $^I = '.bak'; # create a backup copy
 
 while (<>) {
-   s/\"system\"\:{}/\"system\"\:{\"site_code\"\:\"tdf\"}/g; # add site_code to empty records
+   s/\"system\"\:\{\}/\"system\"\:\{\"site_code\"\:\"tdf\"\}/g; # add site_code to empty records
    s/\"site_code\"\:\"tdf\"/\"domain_code\"\:\"tdf\",\"site_code\"\:\"tdf\"/g; # do the replacement
    print; # print to the modified file
 }
